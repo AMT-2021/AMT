3ed606156fa4dd1ca8d3ee26349fd527ae57c631
Roosembert Palacios <roosemberth@posteo.ch>
2021-10-04 09:36:37 +0200

    Bootstrap spring boot project
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

---
+++ src/test/java/ch/heigvd/amt/backend/AmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heigvd.amt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
