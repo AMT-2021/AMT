f145e8badff968a5c88f8392749b214a8a3b8b77
Roosemberth Palacios <roosemberth@users.noreply.github.com>
2021-10-06 10:26:31 +0200

    Add linting to CI (#5)
    * Add mvn plugin to format sources according to google's styleguide
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>
    
    * Lint project files according to google's java styleguide
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>
    
    * Setup 'lint' step on CI
    
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- src/main/java/ch/heigvd/amt/backend/AmtBackendApplication.java
+++ src/main/java/ch/heigvd/amt/backend/AmtBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmtBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmtBackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AmtBackendApplication.class, args);
+  }
 
 }
