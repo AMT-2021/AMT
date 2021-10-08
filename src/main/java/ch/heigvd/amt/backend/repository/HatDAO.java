3ba050bf5b42ad891c0adefba549dae8f34a2d19
PaulReeve717 <paulreeve717@gmail.com>
2021-10-08 19:20:53 +0200

    add Hat repository

---
+++ src/main/java/ch/heigvd/amt/backend/repository/HatDAO.java
@@ -0,0 +1,10 @@
+package ch.heigvd.amt.backend.repository;
+
+import ch.heigvd.amt.backend.DBSchema.Hat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HatDAO extends JpaRepository<Hat, Integer> {
+    Optional<Hat> getHatById(Integer id);
+}
