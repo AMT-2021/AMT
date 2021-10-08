376bb68eeedb3cd639f0c1ae01dd4b8029fb5b08
PaulReeve717 <paulreeve717@gmail.com>
2021-10-08 17:03:06 +0200

    add Hat model

---
+++ src/main/java/ch/heigvd/amt/backend/DBSchema/Hat.java
@@ -0,0 +1,29 @@
+package ch.heigvd.amt.backend.DBSchema;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@ToString
+@EqualsAndHashCode
+public class Hat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    private Integer id;
+
+    @Getter
+    @Setter
+    private String name;
+
+
+}
