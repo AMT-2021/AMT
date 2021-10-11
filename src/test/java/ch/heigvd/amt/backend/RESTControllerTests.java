99cec09fd3eeb37277036998fe6b5cf2346bb0dd
PaulReeve717 <paulreeve717@gmail.com>
2021-10-11 11:52:10 +0200

    RESTControllerTests : Add test file for the controllers

---
+++ src/test/java/ch/heigvd/amt/backend/RESTControllerTests.java
@@ -0,0 +1,37 @@
+package ch.heigvd.amt.backend;
+
+import ch.heigvd.amt.backend.repository.HatDAO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RESTControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private HatDAO hatRepository;
+
+    @Test
+    @Transactional
+    public void createAndSelectHatTest() throws Exception {
+        this.mockMvc.perform(post("/hats")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"name\": \"testHat\" }")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("testHat"));
+    }
+
+}
