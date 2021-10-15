4b3384d090a9f7002f1a9ce7e83c852e8ea6f148
PaulReeve717 <paulreeve717@gmail.com>
2021-10-15 15:41:18 +0200

    Reformat HatService and RESTControllerTests

--- src/main/java/ch/heigvd/amt/backend/routes/HatService.java
+++ src/main/java/ch/heigvd/amt/backend/routes/HatService.java
@@ -14,28 +14,28 @@ import java.util.Optional;
 @RestController
 public class HatService {
 
-    @Autowired
-    private HatDAO hatDAO;
+  @Autowired private HatDAO hatDAO;
 
-    @GetMapping("/hats/{hatId}")
-    public ResponseEntity<Hat> getHatNameById(@PathVariable Integer hatId) {
-        Optional<Hat> hat = hatDAO.getHatById(hatId);
-        if (hat.isPresent()) {
-            return ResponseEntity.ok().body(hat.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+  @GetMapping("/hats/{hatId}")
+  public ResponseEntity<Hat> getHatNameById(@PathVariable Integer hatId) {
+    Optional<Hat> hat = hatDAO.getHatById(hatId);
+    if (hat.isPresent()) {
+      return ResponseEntity.ok().body(hat.get());
+    } else {
+      return ResponseEntity.notFound().build();
     }
+  }
 
-    @PostMapping(path = "/hats",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Hat> createHat(@RequestBody Hat newHat) throws ServerException {
-        Hat hat = hatDAO.save(newHat);
-        if (hat == null) {
-            throw new ServerException("The hat couldn't be created");
-        } else {
-            return new ResponseEntity<>(hat, HttpStatus.CREATED);
-        }
+  @PostMapping(
+      path = "/hats",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Hat> createHat(@RequestBody Hat newHat) throws ServerException {
+    Hat hat = hatDAO.save(newHat);
+    if (hat == null) {
+      throw new ServerException("The hat couldn't be created");
+    } else {
+      return new ResponseEntity<>(hat, HttpStatus.CREATED);
     }
+  }
 }
--- src/test/java/ch/heigvd/amt/backend/RESTControllerTests.java
+++ src/test/java/ch/heigvd/amt/backend/RESTControllerTests.java
@@ -16,22 +16,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RESTControllerTests {
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-    @Autowired
-    private HatDAO hatRepository;
+  @Autowired private HatDAO hatRepository;
 
-    @Test
-    @Transactional
-    public void createAndSelectHatTest() throws Exception {
-        this.mockMvc.perform(post("/hats")
+  @Test
+  @Transactional
+  public void createAndSelectHatTest() throws Exception {
+    this.mockMvc
+        .perform(
+            post("/hats")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{ \"name\": \"testHat\" }")
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.name").value("testHat"));
-    }
-
+        .andExpect(status().isCreated())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.name").value("testHat"));
+  }
 }
