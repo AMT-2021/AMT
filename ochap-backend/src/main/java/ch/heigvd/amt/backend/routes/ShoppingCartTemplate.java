d9dcdde394001362d70ec485a197f3735b96a40f
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-11-08 09:35:02 +0100

    init of shopping-cart

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
@@ -1,43 +1,14 @@
 package ch.heigvd.amt.backend.routes;
 
-import ch.heigvd.amt.backend.entities.Product;
-import ch.heigvd.amt.backend.repository.ProductDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Optional;
 
 @Controller
 public class ShoppingCartTemplate {
-
-  @Autowired private ProductDAO productDAO;
-  //@Autowired private ShoppingCartDAO shoppingCartDAO;
-
-  @GetMapping("/shopping-cart")
-  public String basicTemplate(Model model) {
-
-    Product[] products = new Product[] {getProductById(2).getBody()};
-
-    model.addAttribute("products", products);
-    model.addAttribute("postAddress", "/shopping-cart");
-    return "shopping-cart-template";
-  }
-
-  private ResponseEntity<Product> getProductById(Integer productId) {
-    Optional<Product> product = productDAO.getProductById(productId);
-    return product
-        .map(value -> ResponseEntity.ok().body(value))
-        .orElseGet(() -> ResponseEntity.notFound().build());
-  }
-
-  @PostMapping(path = "/shopping-cart", consumes = MediaType.APPLICATION_JSON_VALUE)
-  public void createEmployee(@RequestBody ProductDAO shoppingCart) {
-
-  }
+    @GetMapping("/shopping-cart")
+    public String basicTemplate(Model model) {
+        //model.addAttribute("title", "Basic template");
+        return "shopping-cart-template";
+    }
 }
