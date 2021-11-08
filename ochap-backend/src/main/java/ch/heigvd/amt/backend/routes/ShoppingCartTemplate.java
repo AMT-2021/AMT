c9e54412647322f0073cd15120f6ede223fa38fc
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-11-08 10:17:53 +0100

    list of product with thymeleaf

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShoppingCartTemplate {
-    @GetMapping("/shopping-cart")
-    public String basicTemplate(Model model) {
-        //model.addAttribute("title", "Basic template");
-        return "shopping-cart-template";
-    }
+  @GetMapping("/shopping-cart")
+  public String basicTemplate(Model model) {
+    String[] items = new String[] { "product1", "product2", "product3", "product4", "product5" };
+    model.addAttribute("items", items);
+    return "shopping-cart-template";
+  }
 }
