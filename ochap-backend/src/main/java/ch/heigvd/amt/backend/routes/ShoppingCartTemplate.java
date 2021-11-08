ac73e4f477f5f845de0ad5244af7d3e31ef1c3fd
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-11-08 11:27:48 +0100

    ajout de form et bouton

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
@@ -8,8 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ShoppingCartTemplate {
   @GetMapping("/shopping-cart")
   public String basicTemplate(Model model) {
-    String[] items = new String[] { "product1", "product2", "product3", "product4", "product5" };
+    Item[] items = new Item[] { new Item("p1",1, 5.2f), new Item("p2",2, 3.4f) };
     model.addAttribute("items", items);
     return "shopping-cart-template";
+  }
+}
+
+class Item {
+  public String name;
+  public int number;
+  public float price;
+
+  public Item(String name, int number, float price) {
+    this.name = name;
+    this.number = number;
+    this.price = price;
   }
 }
