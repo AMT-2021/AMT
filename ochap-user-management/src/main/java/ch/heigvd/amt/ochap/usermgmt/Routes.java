8379ad6743ef49e1bdf011e493611dfd127b3b4a
Roosembert Palacios <roosemberth@posteo.ch>
2021-11-10 09:57:24 +0100

    Add user login form
    Signed-off-by: Roosembert Palacios <roosemberth@posteo.ch>

--- ochap-user-management/src/main/java/ch/heigvd/amt/ochap/usermgmt/Routes.java
+++ ochap-user-management/src/main/java/ch/heigvd/amt/ochap/usermgmt/Routes.java
@@ -1,8 +1,22 @@
 package ch.heigvd.amt.ochap.usermgmt;
 
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.View;
+
+import ch.heigvd.amt.ochap.usermgmt.data.LoginDTO;
 
 @Controller
 public class Routes {
@@ -12,5 +26,33 @@ public class Routes {
     model.addAttribute("explaination", "We're working on it.");
     model.addAttribute("backUrl", "/");
     return "simple-error";
+  }
+
+  @GetMapping("/login")
+  public String loginView(Model model, LoginDTO loginDTO) {
+    model.addAttribute("callbackUrl", "/");
+    return "login";
+  }
+
+  @PostMapping(path = "/login")
+  public String loginPerform(@RequestParam Map<String, String> queryParams,
+      @Valid LoginDTO loginDTO, BindingResult bindingResult, Model model,
+      HttpServletRequest request, HttpServletResponse response) {
+    String callbackUrl = queryParams.getOrDefault("callback", "/");
+    model.addAttribute("callbackUrl", callbackUrl);
+    if (bindingResult.hasErrors()) {
+      return "login";
+    }
+
+    request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.SEE_OTHER);
+    response.addCookie(new Cookie("Authorization", "invalid-login-not-implemented"));
+    return "redirect:" + callbackUrl;
+  }
+
+  @GetMapping("/register")
+  public String registerView(Model model) {
+    model.addAttribute("callbackUrl", "/");
+    model.addAttribute("cancelUrl", "/");
+    return "register";
   }
 }
--- ochap-user-management/src/test/java/ch/heigvd/amt/ochap/usermgmt/ViewsTests.java
+++ ochap-user-management/src/test/java/ch/heigvd/amt/ochap/usermgmt/ViewsTests.java
@@ -1,14 +1,14 @@
 package ch.heigvd.amt.ochap.usermgmt;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,10 +18,33 @@ public class ViewsTests {
 
   @Test
   public void testOupsPage() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/test-oups-page")).andExpect(status().isOk())
+    mvc.perform(get("/test-oups-page")).andExpect(status().isOk())
         .andExpect(content().string(containsString("Test oups page.")))
         .andExpect(content().string(containsString("working on it.")))
         .andExpect(content().string(containsString("form action=\"/\"")))
         .andExpect(content().string(containsString("type=\"submit\" value=\"Go back\"")));
+  }
+
+  @Test
+  public void getLoginReturnsLoginView() throws Exception {
+    this.mvc.perform(get("/login?callback=/foo")).andExpect(view().name("login"));
+  }
+
+  @Test
+  public void loginActionRedirectsAndSetsCookie() throws Exception {
+    this.mvc
+        .perform(post("/login?callback=/foo").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("username", "test-username").param("password", "test-password"))
+        .andExpect(view().name("redirect:/foo"))
+        .andExpect(cookie().value("Authorization", "invalid-login-not-implemented"));
+  }
+
+  @Test
+  public void loginActionValidationFailureContainsErrorMessages() throws Exception {
+    this.mvc
+        .perform(post("/login?callback=/foo").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("username", "").param("password", ""))
+        .andExpect(content().string(containsString("Username cannot be empty")))
+        .andExpect(content().string(containsString("Password cannot be empty")));
   }
 }
