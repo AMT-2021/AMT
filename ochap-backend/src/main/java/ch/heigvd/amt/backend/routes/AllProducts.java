d4929b7ca3b9370582892d940b114810a0456cd3
Kylian Bourcoud <37708074+Klab93@users.noreply.github.com>
2021-11-12 16:34:02 +0100

    Update src/main/java/ch/heigvd/amt/backend/routes/AllProducts.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/AllProducts.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/AllProducts.java
@@ -8,17 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AllProducts {
-
   @GetMapping("/all-products")
-  @ResponseBody
   public String allProduct(Model model, @RequestParam(required = false) String category) {
-
     String[] products;
     if (category != null) {
-      products = new String[] {"product 1", "product 2", "product 3", "product 4"};
+      products =  new String[]{"product 1", "product 2", "product 3", "product 4"};
     } else {
-      products = new String[] {"product 1", "product 2", "product 3", "product 4", "product 5",
-          "product 6"};
+      products = new String[] {"product 1", "product 2", "product 3", "product 4", "product 5", "product 6"};
     }
 
     String[] categories = new String[] {"cat 1", "cat 2", "cat 3", "cat 4", "cat 5", "cat 6"};
