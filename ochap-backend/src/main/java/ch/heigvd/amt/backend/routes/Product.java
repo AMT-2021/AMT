c99de96fbe0de8232696f03921ad7ab90f8665a7
arbroween <arbroween@gmail.com>
2021-11-12 16:58:51 +0100

    Implemented basic Product route and template

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/Product.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/Product.java
@@ -1,7 +1,5 @@
 package ch.heigvd.amt.backend.routes;
 
-import ch.heigvd.amt.backend.repository.ProductDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class Product {
-  @Autowired private ProductDAO productDAO;
-
   @GetMapping("/products/{id}")
   public String basicTemplate(Model model, @PathVariable int id) {
-    var productOptional = productDAO.getProductById(id);
-    if (productOptional.isEmpty()) {
-      return "productNotFound";
-    }
-
-    var product = productOptional.get();
-    model.addAttribute("id", product.getId());
-    model.addAttribute("name", product.getName());
-    model.addAttribute("price", product.getPrice());
-    model.addAttribute("description", product.getDescription());
-    model.addAttribute(
-        "image", "https://via.placeholder.com/720x480?text=This+image+is+not+contractual");
+    model.addAttribute("id", id);
+    model.addAttribute("name", "Product name");
+    model.addAttribute("price", 130);
+    model.addAttribute("description", "The description of this product will make you want to buy it immediately!");
+    model.addAttribute("image", "https://via.placeholder.com/720x480?text=This+image+is+not+contractual");
     return "product";
   }
 }
