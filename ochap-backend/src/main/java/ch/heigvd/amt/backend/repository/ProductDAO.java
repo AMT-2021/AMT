ff863e0cc34e00af1aef8be0544a8854071dc42e
Klab93 <kylian.bourcoud@gmail.com>
2021-11-15 11:37:45 +0100

    add connection to database

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/repository/ProductDAO.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/repository/ProductDAO.java
@@ -1,10 +1,16 @@
 package ch.heigvd.amt.backend.repository;
 
+import ch.heigvd.amt.backend.entities.Category;
 import ch.heigvd.amt.backend.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductDAO extends JpaRepository<Product, Integer> {
+  @Query(value = "SELECT p from Product p")
+  Optional <List<Product>> getAllProducts();
   Optional<Product> getProductById(Integer id);
+  //Optional <List<Product>> getProductsByCategory(Category category);
 }
--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/AllProducts.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/AllProducts.java
@@ -1,20 +1,35 @@
 package ch.heigvd.amt.backend.routes;
 
+import ch.heigvd.amt.backend.entities.Product;
+import ch.heigvd.amt.backend.repository.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 public class AllProducts {
+
+  @Autowired
+  private ProductDAO productDAO;
+
   @GetMapping("/all-products")
   public String allProduct(Model model, @RequestParam(required = false) String category) {
-    String[] products;
+    Optional<List<Product>> hasProducts;
+    Product[] products = new Product[]{};
     if (category != null) {
-      products =  new String[]{"product 1", "product 2", "product 3", "product 4"};
+      // products =  new String[]{"product 1", "product 2", "product 3", "product 4"};
     } else {
-      products = new String[] {"product 1", "product 2", "product 3", "product 4", "product 5", "product 6"};
+      hasProducts = productDAO.getAllProducts();
+      if (hasProducts.isPresent()) {
+        products = hasProducts.get().toArray(new Product[0]);
+      }
     }
 
     String[] categories = new String[] {"cat 1", "cat 2", "cat 3", "cat 4", "cat 5", "cat 6"};
