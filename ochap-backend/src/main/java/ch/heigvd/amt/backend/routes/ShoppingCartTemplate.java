a185f2059d41a3d3578d63652c35af3f1c91f9fa
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-11-15 10:23:08 +0100

    utilisation des products

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
@@ -1,26 +1,40 @@
 package ch.heigvd.amt.backend.routes;
 
+import ch.heigvd.amt.backend.entities.Product;
+import ch.heigvd.amt.backend.repository.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class ShoppingCartTemplate {
+
+  @Autowired
+  private ProductDAO productDAO;
+
   @GetMapping("/shopping-cart")
   public String basicTemplate(Model model) {
-    Item[] items = new Item[] {new Item("p1", 1, 5.2f), new Item("p2", 2, 3.4f)};
-    model.addAttribute("items", items);
+
+    Product[] products = new Product[] {getProductById(2).getBody()};
+
+    model.addAttribute("products", products);
+    model.addAttribute("postAddress", "/shopping-cart/");
     return "shopping-cart-template";
   }
-}
-class Item {
-  public String name;
-  public int number;
-  public float price;
 
-  public Item(String name, int number, float price) {
-    this.name = name;
-    this.number = number;
-    this.price = price;
+  private ResponseEntity<Product> getProductById(Integer productId) {
+    Optional<Product> product = productDAO.getProductById(productId);
+    return product.map(value -> ResponseEntity.ok().body(value))
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
+
+  /*
+   * @PostMapping("/shopping-cart") public void createEmployee(@RequestBody ShoppingCart
+   * shoppingCart) { return ShoppingCartDAO.save(); }
+   */
+
 }
