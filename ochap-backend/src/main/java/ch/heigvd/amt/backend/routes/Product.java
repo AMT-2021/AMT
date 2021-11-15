8b6fd196d0a3f76a30031200b183c68568aa7671
arbroween <arbroween@gmail.com>
2021-11-15 10:36:10 +0100

    Retrieve product from database and deal with product not found

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/Product.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/Product.java
@@ -1,5 +1,8 @@
 package ch.heigvd.amt.backend.routes;
 
+import ch.heigvd.amt.backend.repository.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +10,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class Product {
+  @Autowired
+  private ProductDAO productDAO;
+
   @GetMapping("/products/{id}")
   public String basicTemplate(Model model, @PathVariable int id) {
-    model.addAttribute("id", id);
-    model.addAttribute("name", "Product name");
-    model.addAttribute("price", 130);
-    model.addAttribute("description", "The description of this product will make you want to buy it immediately!");
+    var productOptional = productDAO.getProductById(id);
+    if (productOptional.isEmpty()) {
+      return "productNotFound";
+    }
+
+    var product = productOptional.get();
+    model.addAttribute("id", product.getId());
+    model.addAttribute("name", product.getName());
+    model.addAttribute("price", product.getPrice());
+    model.addAttribute("description", product.getDescription());
     model.addAttribute("image", "https://via.placeholder.com/720x480?text=This+image+is+not+contractual");
     return "product";
   }
