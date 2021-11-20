f3100930786b5ac76f6e048709a95b095b97b31e
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-11-20 15:02:37 +0100

    début adaptation pour connexion à base de donnée

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartTemplate.java
@@ -3,18 +3,21 @@ package ch.heigvd.amt.backend.routes;
 import ch.heigvd.amt.backend.entities.Product;
 import ch.heigvd.amt.backend.repository.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
 @Controller
 public class ShoppingCartTemplate {
 
-  @Autowired
-  private ProductDAO productDAO;
+  @Autowired private ProductDAO productDAO;
+  //@Autowired private ShoppingCartDAO shoppingCartDAO;
 
   @GetMapping("/shopping-cart")
   public String basicTemplate(Model model) {
@@ -22,19 +25,19 @@ public class ShoppingCartTemplate {
     Product[] products = new Product[] {getProductById(2).getBody()};
 
     model.addAttribute("products", products);
-    model.addAttribute("postAddress", "/shopping-cart/");
+    model.addAttribute("postAddress", "/shopping-cart");
     return "shopping-cart-template";
   }
 
   private ResponseEntity<Product> getProductById(Integer productId) {
     Optional<Product> product = productDAO.getProductById(productId);
-    return product.map(value -> ResponseEntity.ok().body(value))
+    return product
+        .map(value -> ResponseEntity.ok().body(value))
         .orElseGet(() -> ResponseEntity.notFound().build());
   }
 
-  /*
-   * @PostMapping("/shopping-cart") public void createEmployee(@RequestBody ShoppingCart
-   * shoppingCart) { return ShoppingCartDAO.save(); }
-   */
+  @PostMapping(path = "/shopping-cart", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void createEmployee(@RequestBody ProductDAO shoppingCart) {
 
+  }
 }
