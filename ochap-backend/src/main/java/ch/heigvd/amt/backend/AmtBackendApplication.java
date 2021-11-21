a9e75ae648aeccefe3264a75767b881465dcf42f
arbroween <arbroween@users.noreply.github.com>
2021-11-21 17:02:23 +0100

    Fix formatting 3
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/AmtBackendApplication.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/AmtBackendApplication.java
@@ -26,9 +26,9 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 public class AmtBackendApplication extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
-    return app.sources(AmtBackendApplication.class);
   public static void main(String[] args) {
-    SpringApplication.run(AmtBackendApplication.class, args);
+
+++ b/ochap-backend/src/main/java/ch/heigvd/amt/backend/repository/CategoryDAO.java
   @Bean
   public JWTVerifier jwtParser(@Value("${authServiceJwtSecret}") String secret) throws Exception {
     return JWT.require(Algorithm.HMAC256(secret)).build();
