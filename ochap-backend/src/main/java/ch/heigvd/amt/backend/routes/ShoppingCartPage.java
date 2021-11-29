099e376b7206d76f5db3afc301542e173a90b9cc
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-11-29 11:22:48 +0100

    supress unused imports

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartPage.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartPage.java
@@ -10,9 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.persistence.EntityManager;
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
