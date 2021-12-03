fb73d63cc5c8284abe058b740c1da39b02bcd96d
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-12-03 17:17:44 +0100

    Changes by PAUL

--- ochap-backend/src/test/java/ch/heigvd/amt/backend/entities/ProductTest.java
+++ ochap-backend/src/test/java/ch/heigvd/amt/backend/entities/ProductTest.java
@@ -21,6 +21,22 @@ public class ProductTest {
   @Autowired
   private ProductDAO productRepository;
 
+
+  @Test
+  @Transactional
+  void canCreateAProductWithoutACategory() {
+    Product p = new Product();
+    p.setName("Mafia");
+    p.setDescription("Godfather Hat");
+    p.setStock(1);
+    p.setPrice(3);
+    p.setCategories(new HashSet<>());
+    productRepository.save(p);
+    Assertions.assertNotNull(p.getId());
+    Assertions.assertNotNull(productRepository.getProductById(p.getId()).get().getCategories());
+    Assertions.assertEquals(productRepository.getProductById(p.getId()).get().getCategories().size(), 0);
+  }
+
   @Test
   @Transactional
   void canCreateAProductInACategory() {
@@ -33,10 +49,43 @@ public class ProductTest {
     p.setDescription("Godfather Hat");
     p.setStock(1);
     p.setPrice(3);
-    p.setCategory(c);
+    HashSet<Category> cats = new HashSet<>();
+    cats.add(c);
+    p.setCategories(cats);
     productRepository.save(p);
     Assertions.assertNotNull(p.getId());
     Assertions.assertNotNull(productRepository.getProductById(p.getId()).get().getCategories());
     Assertions.assertEquals(productRepository.getProductById(p.getId()).get().getCategories().size(), 1);
+  }
+
+  @Test
+  @Transactional
+  void canCreateAProductInMultipleCategories() {
+    Category c = new Category();
+    c.setName("Fedora");
+    categoryRepository.save(c);
+    Assertions.assertNotNull(c.getId());
+
+    Category c2 = new Category();
+    c2.setName("Fedora2");
+    categoryRepository.save(c2);
+    Assertions.assertNotNull(c2.getId());
+
+    Category c3 = new Category();
+    c3.setName("Fedora3");
+    categoryRepository.save(c3);
+    Assertions.assertNotNull(c3.getId());
+
+    Product p = new Product();
+    p.setName("Mafia");
+    p.setDescription("Godfather Hat");
+    p.setStock(1);
+    p.setPrice(3);
+    p.setCategories(new HashSet<>(Arrays.asList(c, c2)));
+    p.getCategories().add(c3);
+    productRepository.save(p);
+    Assertions.assertNotNull(p.getId());
+    Assertions.assertNotNull(productRepository.getProductById(p.getId()).get().getCategories());
+    Assertions.assertEquals(productRepository.getProductById(p.getId()).get().getCategories().size(), 3);
   }
 }
