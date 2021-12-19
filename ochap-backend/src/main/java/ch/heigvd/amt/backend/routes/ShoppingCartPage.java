1fc61fd70bd952981ebc963a5492d8f5e287b323
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-12-19 15:27:01 +0100

    removed unuseful imports

--- ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartPage.java
+++ ochap-backend/src/main/java/ch/heigvd/amt/backend/routes/ShoppingCartPage.java
@@ -9,19 +9,14 @@ import ch.heigvd.amt.backend.repository.ShoppingCartDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
-import java.lang.reflect.Array;
 import java.util.*;
 
 @Controller
