d019e90887f4b0731ca28cebb39347deceb698a9
PaulReeve717 <paulreeve717@gmail.com>
2022-01-11 15:38:51 +0100

    Check if user exists

--- ochap-authentication/src/main/java/com/example/ochapauthentication/repository/UserDAO.java
+++ ochap-authentication/src/main/java/com/example/ochapauthentication/repository/UserDAO.java
@@ -3,6 +3,8 @@ package com.example.ochapauthentication.repository;
 import com.example.ochapauthentication.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserDAO extends JpaRepository<User, Integer> {
     User getUserByUsername(String username);
 }
--- ochap-authentication/src/main/java/com/example/ochapauthentication/routes/AuthenticationController.java
+++ ochap-authentication/src/main/java/com/example/ochapauthentication/routes/AuthenticationController.java
@@ -1,18 +1,30 @@
 package com.example.ochapauthentication.routes;
 
 import com.example.ochapauthentication.commands.AuthLoginCommand;
+import com.example.ochapauthentication.entities.User;
 import com.example.ochapauthentication.repository.RoleDAO;
 import com.example.ochapauthentication.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 public class AuthenticationController {
 
-    UserDAO userRepository; //TODO: Not use Autowired to build
-    RoleDAO roleRepository; //TODO: Not use Autowired to build
+    final UserDAO userRepository;
+    final RoleDAO roleRepository;
+
+    @Autowired
+    public AuthenticationController(UserDAO userRepository, RoleDAO roleRepository){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+
+    }
+
     @PostMapping("/auth/login")
     @ResponseBody
     String loginAutentication(@RequestBody AuthLoginCommand credentials){
