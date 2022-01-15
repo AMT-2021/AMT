7d64ceb09eea5c9b760ff3cc69f6608f426aa64f
PaulReeve717 <paulreeve717@gmail.com>
2022-01-15 13:51:56 +0100

    Basic register functional

--- ochap-authentication/src/main/java/com/example/ochapauthentication/routes/AuthenticationController.java
+++ ochap-authentication/src/main/java/com/example/ochapauthentication/routes/AuthenticationController.java
@@ -10,17 +10,24 @@ import com.example.ochapauthentication.repository.UserDAO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Controller
 public class AuthenticationController {
@@ -39,15 +46,25 @@ public class AuthenticationController {
     @ResponseBody
     String loginAutentication(@RequestBody AuthLoginCommand credentials){
         Optional<User> user = userRepository.getUserByUsername(credentials.getUsername());
-        //TODO
-        return "";
+        if (user.isEmpty()){
+            return "{\"error\" : \"user don't exist\"}";
+        }
+
+        if (hashPassword(credentials.getPassword(), user.get().getSalt())
+            != user.get().getPassword()) {
+            return "{\"error\" : \"wrong password\"}";
+        }
+        return "{\"success\" : \"logged\"}";
     }
 
     @PostMapping(value = "/accounts/register",  produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
     String registerAutentication(@RequestBody AccountRegisterCommand credentials) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
     if (userRepository.getUserByUsername(credentials.getUsername()).isPresent()) {
-      return "{ \"error\" :  \"user already exists\"}";
+      //TODO
     }
 
         User user = new User();
@@ -57,11 +74,11 @@ public class AuthenticationController {
 
         user.setPassword(hashPassword(credentials.getPassword(), salt));
         user.setSalt(salt);
-        Role role = roleRepository.getRoleByName("user"); //FIXME can't retrieve hardcoded row
+        Role role = roleRepository.findByName("user");
         user.setRole(role);
         User userCreated = userRepository.save(user);
 
-        ObjectMapper objectMapper = new ObjectMapper();
+
 
         AccountInfoDTO accountInfo = new AccountInfoDTO();
         accountInfo.setId(userCreated.getId());
