bbe98fde4e0c5b059cc68078ace831956b955d48
PaulReeve717 <paulreeve717@gmail.com>
2022-01-16 16:43:05 +0100

    Add admin creation in service

--- ochap-authentication/src/main/java/com/example/ochapauthentication/routes/AuthenticationController.java
+++ ochap-authentication/src/main/java/com/example/ochapauthentication/routes/AuthenticationController.java
@@ -11,7 +11,6 @@ import com.example.ochapauthentication.entities.User;
 import com.example.ochapauthentication.jwt.JwtTokenUtil;
 import com.example.ochapauthentication.repository.RoleDAO;
 import com.example.ochapauthentication.repository.UserDAO;
-import com.example.ochapauthentication.services.RoleInitService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +35,6 @@ public class AuthenticationController {
   final UserDAO userRepository;
   final RoleDAO roleRepository;
   final ObjectMapper objectMapper;
-  final RoleInitService roleInitService;
 
   final int MIN_USERNAME_LENGTH = 3;
   final int MIN_PASSWORD_LENGTH = 8;
@@ -46,7 +44,6 @@ public class AuthenticationController {
     this.userRepository = userRepository;
     this.roleRepository = roleRepository;
     this.objectMapper = new ObjectMapper();
-    this.roleInitService = new RoleInitService(this.roleRepository);
   }
 
   @Autowired
@@ -133,7 +130,7 @@ public class AuthenticationController {
     return new ResponseEntity<>(objectMapper.writeValueAsString(accountInfo), HttpStatus.CREATED);
   }
 
-  private byte[] hashPassword(String password, byte[] salt) {
+  public static byte[] hashPassword(String password, byte[] salt) {
 
     try {
       MessageDigest md = MessageDigest.getInstance("SHA-512");
@@ -145,7 +142,7 @@ public class AuthenticationController {
     }
   }
 
-  private byte[] generateSalt() {
+  public static byte[] generateSalt() {
     SecureRandom random = new SecureRandom();
     byte[] salt = new byte[16];
     random.nextBytes(salt);
--- ochap-authentication/src/main/java/com/example/ochapauthentication/services/RoleInitService.java
+++ ochap-authentication/src/main/java/com/example/ochapauthentication/services/RoleInitService.java
@@ -1,28 +1,73 @@
 package com.example.ochapauthentication.services;
 
 import com.example.ochapauthentication.entities.Role;
+import com.example.ochapauthentication.entities.User;
 import com.example.ochapauthentication.repository.RoleDAO;
+import com.example.ochapauthentication.repository.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+
+import static com.example.ochapauthentication.routes.AuthenticationController.generateSalt;
+import static com.example.ochapauthentication.routes.AuthenticationController.hashPassword;
 
 @Component
 public class RoleInitService {
 
-  RoleDAO roleRepository;
+  final RoleDAO roleRepository;
+  final UserDAO userRepository;
+  final PlatformTransactionManager ptm;
 
+  @Value("${admin.password}")
+  private String adminPassword;
 
-  public RoleInitService(RoleDAO roleRepository) {
+  @Autowired
+  public RoleInitService(
+      RoleDAO roleRepository, UserDAO userRepository, PlatformTransactionManager ptm) {
     this.roleRepository = roleRepository;
-    if (roleRepository.findByName("user") == null) {
-      Role user = new Role();
-      user.setName("user");
-      roleRepository.save(user);
-    }
+    this.userRepository = userRepository;
+    this.ptm = ptm;
+  }
 
-    if (roleRepository.findByName("admin") == null) {
-      Role admin = new Role();
-      admin.setName("admin");
-      roleRepository.save(admin);
-    }
+  @PostConstruct
+  public void init() {
+    TransactionTemplate tmpl = new TransactionTemplate(ptm);
+    tmpl.execute(
+        new TransactionCallbackWithoutResult() {
+          @Override
+          protected void doInTransactionWithoutResult(TransactionStatus status) {
+
+            if (roleRepository.findByName("user") == null) {
+              Role user = new Role();
+              user.setName("user");
+              roleRepository.save(user);
+            }
+
+            if (roleRepository.findByName("admin") == null) {
+              Role admin = new Role();
+              admin.setName("admin");
+              roleRepository.save(admin);
+            }
+
+            if (userRepository.getUserByUsername("ochap").isEmpty()) {
+              User admin = new User();
+              admin.setUsername("ochap");
+
+              byte[] salt = generateSalt();
+
+              admin.setPasswordHash(hashPassword(adminPassword, salt));
+              admin.setSalt(salt);
+              admin.setRole(roleRepository.findByName("admin"));
+              userRepository.save(admin);
+            }
+          }
+        });
   }
 }
